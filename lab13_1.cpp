c93f54ee8378f61f3ee81b0dec49a73db62a9dfa
buratsakon0 <buratsakon_s@cmu.ac.th>
2024-01-05 09:57:58 +0700

    lab13_1.cpp completed

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -17,3 +17,36 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double A[],int N,double B[]){
+    double x=0, y=0, z=1, a=0;
+    B[4] = A[0];
+    B[5] = A[0];
+    for(int i=0; i<N; i++){
+        
+    //Arithmetic Mean//
+        x += A[i];
+        B[0] = x/N;
+    
+    
+    //Standard Deviation//
+        y += pow(A[i],2);
+        B[1] = sqrt((y / N)-pow(B[0],2));
+    
+    //Geometric Mean//
+        z = z * A[i];
+        B[2] = pow(z, 1.0/N);
+    
+    //Harmonic Mean//
+        a += 1.0/A[i];
+        B[3] = N/a;
+        
+    //Find Max & Min//
+        if(A[i]>B[4]){
+            B[4] = A[i];
+        }
+        if(A[i]<B[5]){
+            B[5] = A[i];
+        }
+    }
+}    
