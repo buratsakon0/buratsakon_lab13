4ceca6f4b495e0d7f05920b5f0ff588f9e94a272
buratsakon0 <buratsakon_s@cmu.ac.th>
2024-01-05 09:58:13 +0700

    lab13_2.cpp completed

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -21,3 +21,26 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+void showImage(const bool A[][M]) {
+    for (int i = 0; i <= M+1 ; ++i) cout << "-";
+    cout << "\n";
+    for (int i = 0; i < N; ++i) {
+        cout << "|";
+        for (int j = 0; j < M; ++j) {
+            if (A[i][j]) cout << "*";
+            else cout << " ";
+        }
+        cout << "|\n";
+    }
+    for (int i = 0; i < M + 2; ++i) cout << "-";
+    cout << "\n";
+}
+void updateImage(bool A[][M], int size, int x, int y) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            if (sqrt(pow(i - x, 2) + pow(j - y, 2)) <= size - 1) {
+                A[i][j] = 1;
+            }
+        }
+    }
+}
